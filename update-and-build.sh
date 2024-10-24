342eb564861bcf6f0aaecce7e4130e2e7c54e951
Rio A <riosyahakbar36@gmail.com>
2024-10-24 17:01:35 +0800

    test

--- update-and-build.sh
+++ update-and-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -x  # Enables script debugging
+set -x  # Enable debugging
+
+# Redirect both stdout and stderr to a log file
+exec > /var/log/update-and-build.log 2>&1
 
 echo "Navigating to the project directory..."
 cd /var/www/html/riodilawedding || { echo "Failed to navigate to project directory"; exit 1; }
@@ -11,4 +14,3 @@ echo "Running Tailwind CSS build..."
 npx tailwindcss -i ./src/css/input.css -o ./public/css/output.css || { echo "Tailwind build failed"; exit 1; }
 
 echo "Script executed successfully"
-
