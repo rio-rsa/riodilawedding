46495dd088a136da3599c3628d6deb6b1283a011
Rio A <riosyahakbar36@gmail.com>
2024-11-06 14:54:10 +0800

    fix buttons

--- public/js/main.js
+++ public/js/main.js
@@ -21,54 +21,39 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function handleTouchFeedback() {
-        // Mobile-specific visual feedback on touch
         scrollButton.addEventListener('touchstart', () => {
             scrollButton.classList.remove('bg-customPrimaryGreen');
-            scrollButton.classList.add('bg-customLightGreen'); // Adds darker background color
+            scrollButton.classList.add('bg-customLightGreen');
         });
 
         scrollButton.addEventListener('touchend', () => {
-            scrollButton.classList.remove('bg-customLightGreen'); // Reverts to original color
+            scrollButton.classList.remove('bg-customLightGreen');
             scrollButton.classList.add('bg-customPrimaryGreen');
         });
     }
 
     function handleResize() {
         if (window.innerWidth >= 960 && !introSection.classList.contains('hidden') && mainContent.classList.contains('hidden')) {
-            // For larger screens: Show both sections side by side
             introSection.classList.remove('fixed', 'inset-0', 'z-50', 'hide');
             introSection.classList.add('relative', 'lg:flex');
             mainContent.classList.remove('hidden');
-            // body.classList.remove('overflow-hidden');
             scrollButton.removeEventListener('click', openInvitation);
-            scrollButton.removeEventListener('touchstart', handleTouchFeedback);
         } else if (window.innerWidth < 960 && !introSection.classList.contains('hidden') && mainContent.classList.contains('hidden')) {
-            // For smaller screens: Enable Open Invitation button
             introSection.classList.add('inset-0', 'z-50', 'overflow-y-auto');
-            // body.classList.add('overflow-hidden');
-            
-            // Mobile-specific "Open Invitation" functionality
             scrollButton.addEventListener('click', openInvitation);
-            handleTouchFeedback();
         } else if (window.innerWidth < 960 && introSection.classList.contains('hidden') && !mainContent.classList.contains('hidden')){
-            // For smaller screens: Enable Open Invitation button
             introSection.classList.add('fixed', 'inset-0', 'z-50');
-            // body.classList.add('overflow-hidden');
             scrollButton.removeEventListener('click', openInvitation);
-            scrollButton.removeEventListener('touchstart', handleTouchFeedback);
         } else if (window.innerWidth >= 960 && introSection.classList.contains('hidden') && !mainContent.classList.contains('hidden')){
             introSection.classList.remove('fixed', 'inset-0', 'z-50', 'hide');
             introSection.classList.add('relative', 'lg:flex');
             scrollButton.removeEventListener('click', openInvitation);
-            scrollButton.removeEventListener('touchstart', handleTouchFeedback);
         } else if (window.innerWidth < 960 && !introSection.classList.contains('hidden') && !mainContent.classList.contains('hidden')){
             mainContent.classList.add('hidden');
             scrollButton.addEventListener('click', openInvitation);
-            handleTouchFeedback();
         }
     }
 
-    // Initialize functionality on page load and window resize
     window.addEventListener('resize', handleResize);
     handleResize();
 });
@@ -88,33 +73,25 @@ function closeModal() {
 
 
 function openGoogleMapsDuri() {
-    // Use Google Maps URL scheme to open Google Maps
-    const url = "https://maps.app.goo.gl/BcoythpWnAngR7hP9"; // Replace with desired coordinates or location
-    window.open(url, '_blank');  // Open in a new tab
+    const url = "https://maps.app.goo.gl/BcoythpWnAngR7hP9";
+    window.open(url, '_blank');
 }
 
 function openGoogleMapsJogja() {
-    // Use Google Maps URL scheme to open Google Maps
-    const url = "https://maps.app.goo.gl/xfDWegCDjTzDt1vs5"; // Replace with desired coordinates or location
-    window.open(url, '_blank');  // Open in a new tab
+    const url = "https://maps.app.goo.gl/xfDWegCDjTzDt1vs5";
+    window.open(url, '_blank');
 }
 
-// Set the date for the wedding in UTC (replace with your wedding date in UTC)
-const duriWeddingUTC = Date.UTC(2024, 11, 1, 1, 0, 0);  // Dec 1, 2024, 01:00:00 UTC
-const jogjaWeddingUTC = Date.UTC(2024, 11, 8, 2, 15, 0);  // Dec 8, 2024, 02:15:00 UTC
+const duriWeddingUTC = Date.UTC(2024, 11, 1, 1, 0, 0);
+const jogjaWeddingUTC = Date.UTC(2024, 11, 8, 2, 15, 0);
 
 function updateCountdown() {
-    // Get current local time and convert to UTC
     const now = new Date();
-
-    // Convert the current local time to UTC in milliseconds
     const nowUTC = now.getTime();
 
-    // Calculate the difference in milliseconds
     const duriTimeDifference = duriWeddingUTC - nowUTC;
     const jogjaTimeDifference = jogjaWeddingUTC - nowUTC;
 
-    // Calculate days, hours, minutes, and seconds remaining
     const duriDays = Math.floor(duriTimeDifference / (1000 * 60 * 60 * 24));
     const duriHours = Math.floor((duriTimeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const duriMinutes = Math.floor((duriTimeDifference % (1000 * 60 * 60)) / (1000 * 60));
@@ -124,7 +101,6 @@ function updateCountdown() {
     const jogjaMinutes = Math.floor((jogjaTimeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const jogjaSeconds = Math.floor((jogjaTimeDifference % (1000 * 60)) / 1000);
 
-    // If the countdown is over, display a message
     if (duriTimeDifference < 0) {
         duriDays = duriHours = duriMinutes = duriSeconds = 0;
     }
@@ -132,7 +108,6 @@ function updateCountdown() {
         jogjaDays = jogjaHours = jogjaMinutes = jogjaSeconds = 0;
     }
 
-    // Update the countdown boxes
     document.getElementById('duriDays').textContent = String(duriDays).padStart(2, '0');
     document.getElementById('duriHours').textContent = String(duriHours).padStart(2, '0');
     document.getElementById('duriMinutes').textContent = String(duriMinutes).padStart(2, '0');
@@ -146,40 +121,30 @@ function updateCountdown() {
 
 }
 
-// Update countdown every second
 setInterval(updateCountdown, 1000);
 
-
-// Function to detect if the user is on iOS
 function isiOS() {
     return /iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
 
-// Function to add event to calendar based on the device
 function addToCalendarDuri() {
     const googleCalendarURL = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Rio+%26+Dila+%28Akad+%26+Resepsi%29&dates=20241201T010000Z/20241201T100000Z&details=Pernikahan+Rio+%26+Dila+%28Akad+%26+Resepsi%29+di+Duri%2C+Riau&location=Gedung+Lancang+Kuning%2C+PT.+Pertamina+Hulu+Rokan%2C+Duri%2C+Riau";
-    const icsFileURL = "/public/assets/duri.ics"; // Create an .ics file for Apple Calendar
-
+    const icsFileURL = "/public/assets/duri.ics";
     if (isiOS()) {
-        // For iPhone (Apple Calendar)
-        window.location.href = icsFileURL; // This will download/open the .ics file
+        window.location.href = icsFileURL;
     } else {
-        // For Android and desktop (Google Calendar)
-        window.open(googleCalendarURL, "_blank"); // Opens Google Calendar in a new tab
+        window.open(googleCalendarURL, "_blank");
     }
 }
 
-// Function to add event to calendar based on the device
 function addToCalendarJogja() {
     const googleCalendarURL = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Acara+Ngunduh+Mantu+Rio+%26+Dila&dates=20241208T021500Z/20241208T043000Z&details=Acara+Ngunduh+Mantu+Rio+%26+Dila+di+Yogyakarta&location=Pendopo+Tulungo%2C+Kasultanan+Ngayogyakarta+Hadiningrat";
-    const icsFileURL = "/public/assets/jogja.ics"; // Create an .ics file for Apple Calendar
+    const icsFileURL = "/public/assets/jogja.ics";
 
     if (isiOS()) {
-        // For iPhone (Apple Calendar)
-        window.location.href = icsFileURL; // This will download/open the .ics file
+        window.location.href = icsFileURL;
     } else {
-        // For Android and desktop (Google Calendar)
-        window.open(googleCalendarURL, "_blank"); // Opens Google Calendar in a new tab
+        window.open(googleCalendarURL, "_blank");
     }
 }
 
@@ -238,16 +203,14 @@ function increasePax() {
 function decreasePax() {
     const paxInput = document.getElementById("pax");
     let paxValue = parseInt(paxInput.value, 10);
-    if (paxValue > 1) { // Prevents going below 1
+    if (paxValue > 1) {
         paxInput.value = paxValue - 1;
     }
 }
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyXT6Izx6mjzESbyUKRnQhcqsDGpKZI7BEIDeAnEGbaBtBtpOq-PAjHqxWdjovT9wQ/exec'
 
-
 const form = document.forms['rsvp-form']
-
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -256,3 +219,12 @@ form.addEventListener('submit', e => {
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
+
+function copyLink() {
+    const link = "https://www.youtube.com/embed/9FiN7fHhdew";
+    navigator.clipboard.writeText(link).then(() => {
+      alert("Tautan berhasil disalin");
+    }).catch(err => {
+      console.error("Failed to copy link: ", err);
+    });
+  }
