8e31f2f04bfefae21fea678197042b0265ab2b79
Rio A <riosyahakbar36@gmail.com>
2024-11-08 22:49:21 +0800

    update to website

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const scrollButton = document.getElementById('scroll-btn');
     const introSection = document.getElementById('intro-section');
